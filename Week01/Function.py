a93cac856d6b9f600cd6103ef634269afba970b2
Asuman R.N Celik <arncelik@gmail.com>
2018-09-27 13:55:02 -0500

    Update Function.py

--- Week01/Function.py
+++ Week01/Function.py
@@ -1,3 +1,11 @@
 '''
-https://www.hackerrank.com/challenges/python-print
+Read an integer n.
+
+Without using any string methods, try to print the following:
+1 2 3......n
+
+Input Format
+The first line contains an integer
+
+source: https://www.hackerrank.com/challenges/python-print
 '''
