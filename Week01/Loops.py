9a8b45f0a1babc8b8f47e43b247b66606b67826b
Asuman R.N Celik <arncelik@gmail.com>
2018-09-27 13:31:07 -0500

    Update and rename .py to Loops.py

---
+++ Week01/Loops.py
@@ -0,0 +1,17 @@
+'''
+Task 
+Read an integer . For all non-negative integers , print . 
+
+Input Format
+The first and only line contains the integer, .
+
+Constraints
+1<n<20
+
+Output Format
+Print n lines, one corresponding to each .
+
+source: https://www.hackerrank.com/challenges/python-loops/problem
+
+'''
+
