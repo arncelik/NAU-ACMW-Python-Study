1218706f8b95c7c3e6e242b95bdd0fd3c79a91a6
ehuseni <43652401+ehuseni@users.noreply.github.com>
2018-09-27 14:06:20 -0500

    Update Function.py

--- Week01/Function.py
+++ Week01/Function.py
@@ -18,3 +18,9 @@ if __name__ == '__main__':
     n = int(input())
     for i in range(1,n+1):
         print (i, end ="")
+#elena
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print (i, end ="")
+
