97b9c1ce9175ccbffd9f279bb727600a19170855
Asuman R.N Celik <arncelik@gmail.com>
2018-09-27 12:21:22 -0500

    Create Division.py

---
+++ Week00/Division.py
@@ -0,0 +1,8 @@
+'''
+Task 
+Read two integers and print two lines. The first line should contain integer division,  // . The second line should contain float division,  / .
+
+You don't need to perform any rounding or formatting operations.
+
+source: HackerRank
+'''
