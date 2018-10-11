02bcdf331f3a4a3e80d827e6be6d2532966ca57d
ehuseni <43652401+ehuseni@users.noreply.github.com>
2018-10-11 13:56:17 -0500

    Update List.py

--- Week02/List.py
+++ Week02/List.py
@@ -2,3 +2,21 @@
 Problem source: https://www.hackerrank.com/challenges/list-comprehensions/problem
 Please write your solution below
 '''
+'''
+ELENA 
+x=int(input("Enter a number:"))
+y=int(input("Enter a number:"))
+z=int(input("Enter a number:"))
+n=int(input("Enter a number:"))
+list=[]
+count=0
+for i in range(x+1):
+  for j in range(y+1):
+    for k in range(z+1):
+      if i+j+k!=n:
+        list.append([])
+        list[count]=[i,j,k]
+        count=count+1
+print (list)
+
+'''
