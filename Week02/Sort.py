6a8fcd8710169471fcd003318073333258f8d471
Asuman R.N Celik <arncelik@gmail.com>
2018-10-11 13:40:01 -0500

    Update Sort.py

--- Week02/Sort.py
+++ Week02/Sort.py
@@ -3,4 +3,15 @@ Problem:
 Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. 
 You are given n scores. 
 Store them in a list and find the score of the runner-up.
+
+Sample Solution:
+n = int(input())
+arr = list(map(int, input().split()))
+first = max(arr)
+i=0
+while(i<n):
+    if first ==max(arr):
+        arr.remove(max(arr))
+    i+=1
+print(max(arr))
 '''
