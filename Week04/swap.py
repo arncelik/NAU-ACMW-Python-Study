9c1fc783cc6be0b0a5f83a3c17a2ae0252c63064
Asuman R.N Celik <arncelik@gmail.com>
2018-11-27 20:06:03 -0600

    Create swap.py

---
+++ Week04/swap.py
@@ -0,0 +1,17 @@
+'''
+Problem:
+
+You are given a string and your task is to swap cases. 
+In other words, convert all lowercase letters to uppercase letters and vice versa.
+
+For example:
+Www.HackerRank.com → wWW.hACKERrANK.COM
+Pythonist 2 → pYTHONIST 2
+
+Input format:
+A single line containing a string s.
+
+Output format:
+Print the modified string .
+
+'''
